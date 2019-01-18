88981e03e18ac4eed8df463c0eda3296d237765b
zpaki5142 <zachisaac@users.noreply.github.com>
2019-01-17 17:28:34 -0800

    update

---
+++ nytreact-app/build/precache-manifest.b5e888e9966ee876ada0765424083424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b36cb307f2f6b2184d7",
+    "url": "/static/js/main.9b36cb30.chunk.js"
+  },
+  {
+    "revision": "9413d79163741e895669",
+    "url": "/static/js/1.9413d791.chunk.js"
+  },
+  {
+    "revision": "9b36cb307f2f6b2184d7",
+    "url": "/static/css/main.bdc193cb.chunk.css"
+  },
+  {
+    "revision": "a63e81acca22db50fd5e33470d91c115",
+    "url": "/index.html"
+  }
+];
